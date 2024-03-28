a6fd7d1f14c328ba14450b917eb2c82943f854c2
kevalrughani <160831212+kevalrughani@users.noreply.github.com>
2024-03-28 12:59:13 +0530

    Add files via upload

---
+++ friend class - Copy.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class ClassB;
+class ClassA
+{
+	private:
+		int numA;
+		friend class ClassB;
+		public:
+			ClassA() : numA(12){}
+ };
+class ClassB
+ {
+ 	private:
+ 		int numB;
+ 		public:
+ 			ClassB():numB(12){}
+			 int add()
+			 {
+			 	ClassA objectA;
+			 	return objectA.numA+numB;
+			 }
+ };
+ int main()
+ {
+ 	ClassB objectB;
+ 	cout<<"Sum:"<<objectB.add();
+ 
+	return 0;
+}
+
